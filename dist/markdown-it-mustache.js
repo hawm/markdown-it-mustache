e49b028dbfab61c387711c92502bef9883458cea
hawm <20796940+hawm@users.noreply.github.com>
2023-09-26 14:38:46 +0000

    update dist

--- dist/markdown-it-mustache.js
+++ dist/markdown-it-mustache.js
@@ -8,146 +8,146 @@ const d = [
   62
   /* > */
 ];
-function h(e, r, i, c) {
-  for (let n = r; n < i; n++)
-    if (!c(e.charCodeAt(n)))
+function f(e, o, i, a) {
+  for (let n = o; n < i; n++)
+    if (!a(e.charCodeAt(n)))
       return !1;
   return !0;
 }
-function C(e, r, i, c = !1) {
-  let n, s, o;
-  for (n = r; n < i - 5; n++) {
+function _(e, o, i, a = !1) {
+  let n, s, r;
+  for (n = o; n < i - 5; n++) {
     if (e.charCodeAt(n) === 123 && e.charCodeAt(n + 1) === 123) {
-      o = !0;
+      r = !0;
       break;
     }
-    if (c || e.charCodeAt(n) === 10)
+    if (a || e.charCodeAt(n) === 10)
       return !1;
   }
-  if (!o)
+  if (!r)
     return !1;
   for (s = n + 2; s <= i; s++)
     if (e.charCodeAt(s - 1) == 125 && e.charCodeAt(s) == 125)
       return { start: n, end: s };
   return !1;
 }
-function f(e, r, i, c = !1) {
-  const n = C(e, r, i, c);
+function m(e, o, i, a = !1) {
+  const n = _(e, o, i, a);
   if (!n)
     return !1;
-  const s = e.slice(n.start, n.end + 1), o = s.slice(2, -2), l = o.charCodeAt(0), u = d.includes(l), a = u ? !1 : l === k, t = u ? 1 : a ? -1 : 0, p = o.slice(t ? 1 : 0).trim(), m = t !== 0 || g.includes(l) ? String.fromCharCode(l) : null;
+  const s = e.slice(n.start, n.end + 1), r = s.slice(2, -2), c = r.charCodeAt(0), u = d.includes(c), l = u ? !1 : c === k, t = u ? 1 : l ? -1 : 0, h = r.slice(t ? 1 : 0).trim(), p = t !== 0 || g.includes(c) ? String.fromCharCode(c) : "";
   return {
     opening: u,
-    closing: a,
+    closing: l,
     nesting: t,
-    mark: m,
-    name: p,
-    content: o,
+    mark: p,
+    name: h,
+    content: r,
     raw: s,
     pos: n
   };
 }
-function b(e, r, i, c) {
-  let n, s, o, l, u = 0;
-  for (o = i; o <= c && (n = e.bMarks[o] + e.tShift[o], s = e.eMarks[o], !(n < s && e.sCount[o] < e.blkIndent)); o++)
-    if (!(e.sCount[o] - e.blkIndent >= 4) && (l = f(e.src, n, s, !0), !!l && h(
+function C(e, o, i, a) {
+  let n, s, r, c, u = 0;
+  for (r = i; r <= a && (n = e.bMarks[r] + e.tShift[r], s = e.eMarks[r], !(n < s && e.sCount[r] < e.blkIndent)); r++)
+    if (!(e.sCount[r] - e.blkIndent >= 4) && (c = m(e.src, n, s, !0), !!c && f(
       e.src,
-      l.pos.end + 1,
-      e.eMarks[o],
+      c.pos.end + 1,
+      e.eMarks[r],
       e.md.utils.isWhiteSpace
     )))
-      if (l.closing && l.name === r.name) {
+      if (c.closing && c.name === o.name) {
         if (u === 0) {
-          l.line = o, l.block = !0;
+          c.line = r, c.block = !0;
           break;
         }
         u--;
       } else
-        l.opening && l.name === r.name && u++;
-  return l;
+        c.opening && c.name === o.name && u++;
+  return c;
 }
-function M(e, r, i, c) {
-  const n = e.bMarks[r] + e.tShift[r], s = e.eMarks[r], o = f(e.src, n, s, !0);
-  if (!o || !o.opening || !h(
+function M(e, o, i, a) {
+  const n = e.bMarks[o] + e.tShift[o], s = e.eMarks[o], r = m(e.src, n, s, !0);
+  if (!r || !r.opening || !f(
     e.src,
-    o.pos.end + 1,
+    r.pos.end + 1,
     s,
     e.md.utils.isWhiteSpace
   ))
     return !1;
-  const l = b(e, o, r + 1, i);
-  if (!l)
-    return !1;
-  if (!c) {
-    const u = e.push("mustache_open", "", 1);
-    u.block = !0, u.meta = { tag: o }, u.map = [r, l.line];
-    const a = e.parentType, t = e.lineMax;
-    e.parentType = "mustache_block", e.lineMax = l.line, e.md.block.tokenize(e, r + 1, l.line), e.parentType = a, e.lineMax = t;
-    const p = e.push("mustache_close", "", -1);
-    p.block = !0;
-  }
-  return e.line = l.line + 1, !0;
-}
-function _(e, r, i, c, n) {
-  let s = e[r];
-  if (s.nesting === -1)
-    return "</details>";
-  let o = s.meta.tag;
-  return `<details><summary>${o.mark}${o.name}</summary>`;
-}
-function y(e, r = _) {
-  e.block.ruler.after("fence", "mustache_block", M, {
-    alt: ["paragraph", "reference", "blockquote", "list"]
-  }), e.renderer.rules.mustache_open = r, e.renderer.rules.mustache_close = r;
-}
-function I(e, r, i, c) {
-  let n, s = 0;
-  for (let o = i; o < c - 5; ) {
-    if (n = f(e, o, c), !n)
-      return !1;
-    if (n.closing && n.name === r.name) {
-      if (s === 0)
-        break;
-      s--;
-    } else
-      n.opening && n.name === r.name && s++;
-    o = n.pos.end + 1;
-  }
-  return n;
-}
-function T(e, r) {
-  let i;
-  const c = f(e.src, e.pos, e.posMax);
+  const c = C(e, r, o + 1, i);
   if (!c)
     return !1;
-  if (c.nesting === 0)
-    return r || (i = e.push("mustache_inline", "", 0), i.meta = { tag: c }), e.pos = c.pos.end + 1, !0;
-  if (c.closing)
-    return r || (i = e.push("mustache_inline", "", 0), i.meta = { tag: c }), e.pos = c.pos.end + 1, !0;
-  const n = I(
+  if (!a) {
+    const u = e.push("mustache_open", "", 1);
+    u.block = !0, u.meta = { tag: r }, u.map = [o, c.line];
+    const l = e.parentType, t = e.lineMax;
+    e.parentType = "mustache_block", e.lineMax = c.line, e.md.block.tokenize(e, o + 1, c.line), e.parentType = l, e.lineMax = t;
+    const h = e.push("mustache_close", "", -1);
+    h.block = !0;
+  }
+  return e.line = c.line + 1, !0;
+}
+function b(e, o, i, a, n) {
+  const s = e[o], r = s.tag || "div";
+  if (s.nesting === -1)
+    return `</${r}>`;
+  const c = s.meta.tag;
+  return `<${r} class="mustache mustache-block" data-mustache-name=${c.name}   data-mustache-mark=${c.mark}>`;
+}
+function I(e, o = b) {
+  e.block.ruler.after("fence", "mustache_block", M, {
+    alt: ["paragraph", "reference", "blockquote", "list"]
+  }), e.renderer.rules.mustache_open = o, e.renderer.rules.mustache_close = o;
+}
+function $(e, o, i, a) {
+  let n, s = 0;
+  for (let r = i; r < a - 5; r = n.pos.end + 1) {
+    if (n = m(e, r, a), !n)
+      return !1;
+    if (n.closing && n.name === o.name) {
+      if (s === 0)
+        return n;
+      s--;
+    } else
+      n.opening && n.name === o.name && s++;
+  }
+  return !1;
+}
+function x(e, o) {
+  let i;
+  if (e.src.charCodeAt(e.pos) !== 123)
+    return !1;
+  const a = m(e.src, e.pos, e.posMax);
+  if (!a)
+    return !1;
+  if (a.nesting === 0)
+    return o || (i = e.push("mustache_inline", "span", 0), i.meta = { tag: a }), e.pos = a.pos.end + 1, !0;
+  if (a.closing)
+    return o || (i = e.push("mustache_inline", "span", 0), i.meta = { tag: a }), e.pos = a.pos.end + 1, !0;
+  const n = $(
     e.src,
-    c,
-    c.pos.end + 1,
+    a,
+    a.pos.end + 1,
     e.posMax
   );
-  return n ? (r || (i = e.push("mustache_inline_block", "", 0), i.children = [], e.md.inline.parse(
-    e.src.slice(c.pos.end + 1, n.pos.start),
-    e.md,
-    {},
-    i.children
-  )), e.pos = n.pos.end + 1, !0) : (r || (i = e.push("mustache_inline", "", 0), i.meta = { tag: c }), e.pos = c.pos.end + 1, !0);
+  return n ? (o || (i = e.push("mustache_inline_open", "span", 1), i.meta = { tag: a }, e.pos = a.pos.end + 1, e.posMax = n.pos.start, e.md.inline.tokenize(e), e.pos = n.pos.end + 1, e.posMax = e.src.length, e.push("mustache_inline_close", "span", -1)), e.pos = n.pos.end + 1, !0) : (o || (i = e.push("mustache_inline", "span", 0), i.meta = { tag: a }), e.pos = a.pos.end + 1, !0);
 }
-function x(e, r, i, c, n) {
-  return `<span>${e[r].meta.tag.raw}</span>`;
+function T(e, o, i, a, n) {
+  const s = e[o], r = s.tag || "span", c = s.meta.tag;
+  return `<${r} class="mustache mustache-inline" data-mustache-name="${c.name}"   data-mustache-mark="${c.mark}"></${r}>`;
 }
-function A(e, r, i, c, n) {
-  const s = e[r];
-  return `<span>${n.renderInline(s.children, i, c)}</span>`;
+function A(e, o, i, a, n) {
+  const s = e[o], r = s.tag || "span";
+  if (s.nesting === -1)
+    return `</${r}>`;
+  const c = s.meta.tag;
+  return `<${r} class="mustache mustache-inline mustache-inline-block"   data-mustache-name="${c.name}" data-mustache-mark=${c.mark}>`;
 }
-function B(e, r = x, i = A) {
-  e.inline.ruler.push("mustache_inline", T), e.renderer.rules.mustache_inline = r, e.renderer.rules.mustache_inline_block = i;
+function O(e, o = T, i = A) {
+  e.inline.ruler.push("mustache_inline", x), e.renderer.rules.mustache_inline = o, e.renderer.rules.mustache_inline_open = i, e.renderer.rules.mustache_inline_close = i;
 }
 export {
-  y as mustacheBlock,
-  B as mustacheInline
+  I as mustacheBlock,
+  O as mustacheInline
 };
